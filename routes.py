45e1c8b429fed1ad2b46a3d3d7d8e811496920c8
Abu_abdurrahman <99020315+IbnMuhd@users.noreply.github.com>
2025-02-13 07:20:33 +0100

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,1 +1,21 @@
+from flask import render_template, redirect, url_for, request, flash
+from flask_login import login_user, logout_user, login_required
+from app import app, db
+from models import User, Exam, Question
+
+@app.route('/')
+def home():
+    return render_template('login.html')
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    exams = Exam.query.all()
+    return render_template('dashboard.html', exams=exams)
+
+@app.route('/exam/<int:exam_id>')
+@login_required
+def take_exam(exam_id):
+    questions = Question.query.filter_by(exam_id=exam_id).all()
+    return render_template('exam.html', questions=questions)
 
