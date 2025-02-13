0cbe5bdaf682908205bc978f6605bbfc8dca0c7b
Abu_abdurrahman <99020315+IbnMuhd@users.noreply.github.com>
2025-02-13 07:19:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+socketio = SocketIO(app)
+
+from routes import *
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
 
